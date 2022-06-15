f2862b0a7d80f4105d775d9c59f33e22c84b1057
James Wanyoike Mburu <jmburu280@gmail.com>
2022-06-15 11:15:31 +0300

    commit tree a1b1f414c1f4e381bbc22a1f80d3f29b8e9e0b9a

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+input = document.getElementById('input');
+            // Selecting the elelment "input"
+function clickAlert() {
+    //adding event listener and binding it
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
 }
